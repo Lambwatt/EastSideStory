48ee33b5327c39fe2471dc6303c4a28f6c625d2c
Michaelpattie <mjpattie@gmail.com>
2021-02-17 14:18:35 -0800

    Added a bunch of character portraits

---
+++ techtest-unity-master/Assets/Scripts/ScriptableObjects/Personality.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Personality", order = 1)]
+public class Personality : ScriptableObject
+{
+    [SerializeField] string _id;
+    [SerializeField] Sprite _portrait;
+    [SerializeField] string _name;
+    [SerializeField] string _intro;
+    [SerializeField] string _outro;
+    [SerializeField] string[] _winTaunts;
+    [SerializeField] string[] _loseTaunts;
+    [SerializeField] string[] _tieTaunts;
+}
