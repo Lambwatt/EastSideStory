69c976f81dc98314547d3c2db3a9a0d1cd9729df
Michaelpattie <mjpattie@gmail.com>
2021-02-18 15:12:48 -0800

    Added opponent swap animations.

--- techtest-unity-master/Assets/Scripts/Core/OpponentManager.cs
+++ techtest-unity-master/Assets/Scripts/Core/OpponentManager.cs
@@ -10,21 +10,26 @@ public class OpponentManager : MonoBehaviour
     [SerializeField] Text Name;
 
     [SerializeField] List<Personality> Personas;
+    [SerializeField] Animator PortraitAnimator;
 
     Opponent _activeOpponent;
-    OpponentFactory _factory;
+    OpponentFactory _factory; 
 
     public void intialize()
     {
         _factory = new OpponentFactory(Personas);
         ChooseOpponent();
+        Speech.text = "";
+        PlayEnterAnimation(() =>
+        {
+            Speech.text = _activeOpponent.GetIntro();
+        });
     }
 
     void ChooseOpponent()
     {
         _activeOpponent = _factory.ChooseOpponent();
-        Portrait.sprite = _activeOpponent.GetPortrait();
-        Speech.text = _activeOpponent.GetIntro();
+        Portrait.sprite = _activeOpponent.GetPortrait();       
         Name.text = _activeOpponent.GetName();
     }
 
@@ -37,9 +42,8 @@ public class OpponentManager : MonoBehaviour
     {
         if (_activeOpponent.isDoneAfterResult(result))
         {
-            //Just swap opponents here
             Speech.text = _activeOpponent.GetOutro();
-            StartCoroutine(SwapOpponents(OnOpponentSettled));
+            PlayExitAnimation(()=> { SwapOpponents(OnOpponentSettled); });
         }
         else
         {
@@ -52,17 +56,21 @@ public class OpponentManager : MonoBehaviour
         } 
     }
 
-    IEnumerator SwapOpponents(System.Action OnSwapComplete)
+    void SwapOpponents(System.Action OnSwapComplete)
     {
-        yield return new WaitForSeconds(2);
+        //yield return new WaitForSeconds(2);
         ChooseOpponent();
-        Speech.text = _activeOpponent.GetIntro();
-        OnSwapComplete();
+        Speech.text = "";
+        PlayEnterAnimation(() => {
+            Speech.text = _activeOpponent.GetIntro();
+            OnSwapComplete();
+        });
+        
     }
 
     void Taunt(Result r)
     {
-        if (Random.Range(0, 2) == 0)
+        if (UnityEngine.Random.Range(0, 2) == 0)
         {
             string taunt = _activeOpponent.GetTaunt(r);
             if (taunt != null && taunt.Length > 0)
@@ -71,4 +79,24 @@ public class OpponentManager : MonoBehaviour
             }
         }
     }
+
+    void PlayEnterAnimation(System.Action OnAnimationComplete = null)
+    {
+        PortraitAnimator.SetTrigger("In");
+        StartCoroutine(WaitForAnimation(1.2f, OnAnimationComplete));
+    }
+
+    void PlayExitAnimation(System.Action OnAnimationComplete = null)
+    {
+        PortraitAnimator.SetTrigger("Out");
+        StartCoroutine(WaitForAnimation(1.2f, OnAnimationComplete));
+    }
+
+    IEnumerator WaitForAnimation(float animationTime, System.Action OnAnimationComplete = null)
+    {
+        yield return new WaitForSeconds(animationTime);
+        if(OnAnimationComplete!=null)
+            OnAnimationComplete();
+    }
+
 }
